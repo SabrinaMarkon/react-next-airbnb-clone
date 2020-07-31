a9b0f3b591f6b4a5236cddc9509a67be76d441bf
Sabrina Markon <phpsitescripts@outlook.com>
2020-07-31 02:11:42 -0600

    Add global styles to Layout component.

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React from "react";
 
-const Layout = props => {
-    // the props.content property comes from the content prop
-    // passed to Layout from its parent components.
-    return (
-        <div>
-            <main>{props.content}</main>
-        </div>
-    )
-}
+const Layout = (props) => {
+  // the props.content property comes from the content prop
+  // passed to Layout from its parent components.
+  return (
+    <div>
+      <main>{props.content}</main>
+      {/* Global Styles */}
+      <style jsx global>{`
+        body {
+          margin: 0;
+          font-family: Roboto, -apple-system, BlinkMacSystemFont, Segoe UI,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+          font-size: 14px;
+          line-height: 1.5;
+          color: #333;
+        }
+      `}</style>
+      {/* Styles just for main tag */}
+      <style jsx>{`
+        main {
+          position: relative;
+          max-width: 56em;
+          background-color: white;
+          padding: 2em;
+          margin: 0 auto;
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
+};
 
 export default Layout;
