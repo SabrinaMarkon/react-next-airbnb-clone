9f8c503538740ef30badc27ae19a1219f9ae44f6
Sabrina Markon <phpsitescripts@outlook.com>
2020-07-30 23:52:32 -0600

    Add House component and show data about each house on main page.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import houses from './houses.json';
+import House from '../components/House';
 
-const Index = () => {
-    return (
-        <div>
-            <h1>Welcome to NextBNB!</h1>
-        </div>
-    )
-}
+const Index = () => (
+  <div>
+    <h1>Welcome to NextBNB!</h1>
+    <div className="houses">
+      {houses.map((house, index) => {
+        return <House key={index} {...house} />
+      })}
+    </div>
+  </div>
+);
 
 export default Index;
