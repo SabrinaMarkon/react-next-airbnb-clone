6607e451bf57f4d8285c79292ec61f177084642d
Sabrina Markon <phpsitescripts@outlook.com>
2020-08-02 23:49:15 -0600

    Forgot to import useState. Need to export named function in DateRangePicker.

--- components/DateRangePicker.js
+++ components/DateRangePicker.js
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import DayPickerInput from "react-day-picker/DayPickerInput";
+import { DateUtils } from "react-day-picker";
 import dateFnsFormat from "date-fns/format";
 import dateFnsParse from "date-fns/parse";
 
@@ -16,66 +18,70 @@ const formatDate = (date, format, locale) =>
 
 const format = "dd MMM yyyy";
 
-export default () => {
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
+const DateRangePicker = () => {
+  const [startDate, setStartDate] = useState(new Date())
+  const [endDate, setEndDate] = useState(new Date())
 
-  <div className="date-range-picker-container">
-    <div>
-      <label>From:</label>
-      <DayPickerInput
-        formatDate={formatDate}
-        format={format}
-        parseDate={parseDate}
-        placeholder={`${dateFnsFormat(new Date(), format)}`}
-        dayPickerProps={{
-          modifiers: {
-            disabled: {
-              before: new Date(),
-            },
-          },
-        }}
-        onDayChange={(day) => {
-          setStartDate(day);
-        }}
-      />
+  return (
+    <div className='date-range-picker-container'>
+      <div>
+        <label>From:</label>
+        <DayPickerInput
+          formatDate={formatDate}
+          format={format}
+          parseDate={parseDate}
+          placeholder={`${dateFnsFormat(new Date(), format)}`}
+          dayPickerProps={{
+            modifiers: {
+              disabled: {
+                before: new Date()
+              }
+            }
+          }}
+          onDayChange={day => {
+            setStartDate(day)
+          }}
+        />
+      </div>
+      <div>
+        <label>To:</label>
+        <DayPickerInput
+          formatDate={formatDate}
+          format={format}
+          parseDate={parseDate}
+          placeholder={`${dateFnsFormat(new Date(), format)}`}
+          dayPickerProps={{
+            modifiers: {
+              disabled: {
+                before: new Date()
+              }
+            }
+          }}
+          onDayChange={day => {
+            setEndDate(day)
+          }}
+        />
+      </div>
+      <style jsx>{`
+        .date-range-picker-container div {
+          display: grid;
+          border: 1px solid #ddd;
+          grid-template-columns: 30% 70%;
+          padding: 10px;
+        }
+        label {
+          padding-top: 10px;
+        }
+      `}</style>
+      <style jsx global>{`
+        .DayPickerInput input {
+          width: 120px;
+          padding: 10px;
+          font-size: 16px;
+        }
+      `}</style>
     </div>
-    <div>
-      <label>To:</label>
-      <DayPickerInput
-        formatDate={formatDate}
-        format={format}
-        parseDate={parseDate}
-        placeholder={`${dateFnsFormat(new Date(), format)}`}
-        dayPickerProps={{
-          modifiers: {
-            disabled: {
-              before: new Date(),
-            },
-          },
-        }}
-        onDayChange={(day) => {
-          setEndDate(day);
-        }}
-      />
-    </div>
-    <style jsx>{`
-      .date-range-picker-container div {
-        display: grid;
-        border: 1px solid #ddd;
-        grid-template-columns: 30% 70%;
-        padding: 10px;
-      }
-      label {
-        padding-top: 10px;
-      }
-    `}</style>
-    <style jsx global>{`
-      .DayPickerInput input {
-        width: 120px;
-        padding: 10px;
-        font-size: 16px;
-      }
-    `}</style>
-  </div>;
+  );
 };
+
+export default DateRangePicker;
