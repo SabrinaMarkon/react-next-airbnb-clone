50920e2e001e34a137f69d09a2fbe87c753ae84a
Sabrina Markon <phpsitescripts@outlook.com>
2020-08-02 23:23:03 -0600

    Configure date inputs. Modifier that disables calendar days before today.

--- components/DateRangePicker.js
+++ components/DateRangePicker.js
@@ -1,17 +1,47 @@
 import DayPickerInput from "react-day-picker/DayPickerInput";
+import dateFnsFormat from "date-fns/format";
+import dateFnsParse from "date-fns/parse";
+
 // Next.js needs @zeit/next-css installed to know how to handle imported CSS files.
 // Add a next.config.js in the root to support it.
 import "react-day-picker/lib/style.css";
+
+const parseDate = (str, format, locale) => {
+  const parsed = dateFnsParse(str, format, new Date(), { locale });
+  return DateUtils.isDate(parsed) ? parsed : null;
+};
+
+const formatDate = (date, format, locale) =>
+  dateFnsFormat(date, format, { locale });
+
+const format = "dd MMM yyyy";
 
 export default () => (
   <div className="date-range-picker-container">
     <div>
       <label>From:</label>
-      <DayPickerInput />
+      <DayPickerInput
+        formatDate={formatDate}
+        format={format}
+        parseDate={parseDate}
+        placeholder={`${dateFnsFormat(new Date(), format)}`}
+        dayPickerProps={{
+          modifiers: {
+            disabled: {
+              before: new Date(),
+            },
+          },
+        }}
+      />
     </div>
     <div>
       <label>To:</label>
-      <DayPickerInput />
+      <DayPickerInput
+        formatDate={formatDate}
+        format={format}
+        parseDate={parseDate}
+        placeholder={`${dateFnsFormat(new Date(), format)}`}
+      />
     </div>
     <style jsx>{`
       .date-range-picker-container div {
