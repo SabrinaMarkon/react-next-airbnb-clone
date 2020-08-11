e75b10cefa0e445f361baf21ce337c89b556cb47
Sabrina Markon <phpsitescripts@outlook.com>
2020-08-10 23:58:38 -0600

    Display price per night when a date is chosen.

--- pages/houses/[id].js
+++ pages/houses/[id].js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Head from "next/head";
 import houses from "../houses.json";
 import Layout from "../../components/Layout";
@@ -8,6 +9,10 @@ const House = (props) => {
   // We can't assign the JSX to a content variable like we do
   // in index.js because we need to access the specific props
   // for this individual house.
+
+  // Boolean to check if a date range is chosen so we know whether to display the price.
+  const [dateChosen, setDateChosen] = useState(false);
+
   return (
     <Layout
       content={
@@ -29,9 +34,16 @@ const House = (props) => {
             <h2>Add dates for prices</h2>
             <DateRangePicker
               datesChanged={(startDate, endDate) => {
-                console.log(startDate, endDate);
+                // console.log(startDate, endDate);
+                setDateChosen(true);
               }}
             />
+            {dateChosen && (
+              <div>
+                <h2>Price per night</h2>
+                <p>${props.house.price}</p>
+              </div>
+            )}
           </aside>
           <style jsx>{`
             .container {
