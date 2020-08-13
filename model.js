b480a708b740a7a45a8f64adddcdad98e1cbcdd3
Sabrina Markon <phpsitescripts@outlook.com>
2020-08-13 02:08:53 -0600

    Add hook to second argument of User object to encrypt passwords, and isPasswordValid to User prototype to compare during login.

--- model.js
+++ model.js
@@ -1,5 +1,6 @@
 import { Sequelize, Model, DataTypes } from "sequelize";
 import { user, password, host, database } from "./database.js";
+import bcrypt from "bcrypt";
 
 const sequelize = new Sequelize(database, user, password, {
   host,
@@ -26,7 +27,18 @@ User.init(
     sequelize,
     modelName: "user",
     timestamps: false, // Our app doesn't include createdAt or updatedAt fields.
+    hooks: {
+      // Automatically encrypt passwords.
+      beforeCreate: async (user) => {
+        const saltRounds = 10;
+        const salt = await bcrypt.genSalt(saltRounds);
+        user.password = await bcrypt.hash(user.password, salt);
+      },
+    },
   }
 );
 
-
+// Add isPasswordValid new method to User object prototype so we can compare passwords when users login.
+User.prototype.isPasswordValid = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
