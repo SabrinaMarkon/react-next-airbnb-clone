a06cd367b2abcfd57e266331e720e72cbb81a1c0
Sabrina Markon <phpsitescripts@outlook.com>
2020-08-16 00:06:31 -0600

    Add Express server.js.

---
+++ server.js
@@ -0,0 +1,23 @@
+// We use require instead of import because server.js needs to use the CommonJS syntax 
+// to use modules instead of ES Modules, because this file is not going through the 
+// webpack pipeline that Next.js usually does (Copes, F.).
+const express = require("express");
+const next = require("next");
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
+
+nextApp.prepare().then(() => {
+  const server = express();
+
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
