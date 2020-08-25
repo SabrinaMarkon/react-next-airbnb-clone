467ed06ae36831218a899984fdde5ed02df9d2b3
Sabrina Markon <phpsitescripts@outlook.com>
2020-08-25 03:41:39 -0600

    Add server.use to server.js to tell Express to use the session middleware, express-session.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // to use modules instead of ES Modules, because this file is not going through the 
 // webpack pipeline that Next.js usually does (Copes, F.).
 const express = require("express");
+const session = require("express-session");
 const next = require("next");
 
 const port = parseInt(process.env.PORT, 10) || 3000;
@@ -10,7 +11,22 @@ const nextApp = next({ dev });
 const handle = nextApp.getRequestHandler();
 
 nextApp.prepare().then(() => {
+
   const server = express();
+
+  // middleware for handling sessions:
+  server.use(
+    session({
+      secret: 'magpiescrowspigeonssparrowschickadeesgracklesbluejaysravensseagullsbudgiescockatooscockatielsstarlingsflickers', // random string
+      resave: false,
+      saveUninitialized: true,
+      name: 'nextbnb',
+      cookie: {
+        secure: false, // CRITICAL on localhost (Copes, F.)
+        maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
+      }
+    })
+  );
 
   server.all("*", (req, res) => {
     return handle(req, res);
