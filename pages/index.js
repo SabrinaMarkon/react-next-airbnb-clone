258f2c85d0e73c40fb52add228c324fde039ccac
Sabrina Markon <phpsitescripts@outlook.com>
2020-09-05 04:34:45 -0600

    Add getInitialProps for index.js. Move content directly into JSX or Next.js won't call getInitialProps.

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,44 @@
+import fetch from "isomorphic-unfetch";
 import Head from "next/head";
-import houses from "./houses.json";
 import House from "../components/House";
 import Layout from "../components/Layout";
 
-const content = (
-  <div>
-    <Head>
-      <title>NextBNB</title>
-    </Head>
-    <h1>Welcome to NextBNB!</h1>
-    <div className="houses">
-      {houses.map((house, index) => {
-        return <House key={index} {...house} />;
-      })}
-    </div>
-    <style jsx>{`
-      .houses {
-        display: grid;
-        grid-template-columns: 50% 50%;
-        grid-template-rows: 300px 300px;
-        grid-gap: 40px;
+const Index = (props) => {
+  return (
+    <Layout
+      content={
+        <div>
+          <Head>
+            <title>NextBNB</title>
+          </Head>
+          <h1>Welcome to NextBNB! Places to stay:</h1>
+          <div className="houses">
+            {props.houses.map((house, index) => {
+              return <House key={index} {...house} />;
+            })}
+          </div>
+          <style jsx>{`
+            .houses {
+              display: grid;
+              grid-template-columns: 50% 50%;
+              grid-template-rows: 300px 300px;
+              grid-gap: 40px;
+            }
+          `}</style>
+        </div>
       }
-    `}</style>
-  </div>
-);
+    />
+  );
+};
 
-const Index = () => <Layout content={content} />;
+Index.getInitialProps = async () => {
+  // domain and port hardcoded because full path is needed. 
+  // Extract to env variable instead.
+  const res = await fetch("http://localhost:3000/api/houses");
+  const houses = await res.json();
+  return {
+    houses,
+  };
+};
 
 export default Index;
