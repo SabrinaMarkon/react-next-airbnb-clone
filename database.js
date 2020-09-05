00b919701e85fb0967a1c00bbdddba09f64a4870
Sabrina Markon <phpsitescripts@outlook.com>
2020-09-04 23:30:44 -0600

    Move Sequelize from model.js to database.js.

--- database.js
+++ database.js
@@ -1,8 +1,14 @@
-const Database = {
-    user: 'DATABASE_USER',
-    password: 'DATABASE_PASSWORD',
-    host: 'localhost',
-    database: 'DATABASE_NAME'
-}
+const user = 'DATABASE_USER';
+const password = 'DATABASE_PASSWORD';
+const host = 'localhost';
+const database = 'DATABASE_NAME';
 
-module.exports = Database;
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(database, user, password, {
+  host,
+  dialect: "postgres", // can be other kinds of databases not just postgres.
+  logging: false, // verbose - set to true to debug.
+});
+
+module.exports = sequelize;
