f3ab9fd2b74f5c5fe3bcd4467c90b787b92af8d3
Sabrina Markon <phpsitescripts@outlook.com>
2020-09-10 19:37:11 -0600

    Create Booking model.

---
+++ models/booking.js
@@ -0,0 +1,38 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../database.js");
+const DataTypes = Sequelize.DataTypes;
+
+class Booking extends Sequelize.Model {}
+
+Booking.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        houseId: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        startDate: {
+            type: DataTypes.DATEONLY,
+            allowNull: false
+        },
+        endDate: {
+            type: DataTypes.DATEONLY,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        modelName: "booking",
+        timestamps: true
+    }
+);
+
+module.exports = Booking;
