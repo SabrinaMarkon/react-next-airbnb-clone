f96f1f106080297ce5981a75878a1364b4e74cc9
Sabrina Markon <phpsitescripts@outlook.com>
2020-09-17 21:38:46 -0600

    Add bookings data to bookings.js page.

--- pages/bookings.js
+++ pages/bookings.js
@@ -1,7 +1,66 @@
 import Layout from "../components/Layout";
 
+const NEXT_PUBLIC_DOMAIN_URL = process.env.NEXT_PUBLIC_DOMAIN_URL;
+
 const Bookings = () => {
-  return <Layout content={<p>TODO</p>} />;
+  return (
+    <Layout
+      content={
+        <div>
+          <Head>
+            <title>Your bookings</title>
+          </Head>
+          <h2>Your bookings</h2>
+
+          <div className="bookings">
+            {props.bookings.map((booking, index) => {
+              return (
+                <div className="booking" key={index}>
+                  <img src={booking.house.picture} alt="House picture" />
+                  <div>
+                    <h2>
+                      {booking.house.title} in {booking.house.town}
+                    </h2>
+                    <p>
+                      Booked from{" "}
+                      {new Date(booking.booking.startDate).toDateString()} to{" "}
+                      {new Date(booking.booking.endDate).toDateString()}
+                    </p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+
+          <style jsx>{`
+            .bookings {
+              display: grid;
+              grid-template-columns: 100%;
+              grid-gap: 40px;
+            }
+
+            .booking {
+              display: grid;
+              grid-template-columns: 30% 70%;
+              grid-gap: 40px;
+            }
+
+            .booking img {
+              width: 180px;
+            }
+          `}</style>
+        </div>
+      }
+    />
+  );
 };
+
+// Get the bookings prop to use in the JSX above:
+Bookings.getInitialProps = async ctx => {
+    const response = await axios.get(`${NEXT_PUBLIC_DOMAIN_URL}/api/bookings/list`);
+    return {
+        bookings: response.data
+    }
+}
 
 export default Bookings;
