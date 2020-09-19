365fb045e8a57529ddc156150e82d69f6fe3e0e8
Sabrina Markon <phpsitescripts@outlook.com>
2020-09-18 23:54:33 -0600

    Create api/host/list server endpoint to get user's host houses and bookings.

--- server.js
+++ server.js
@@ -506,6 +506,68 @@ nextApp.prepare().then(() => {
     });
   });
 
+  server.get("/api/host/list", async (req, res) => {
+    // Check if a user is logged in using passport so we can show their bookings:
+    if (!req.session.passport || !req.session.passport.user) {
+      res.writeHead(403, {
+        "Content-Type": "application/json",
+      });
+      res.end(
+        JSON.stringify({
+          status: "error",
+          message: "Unauthorized",
+        })
+      );
+
+      return;
+    }
+
+    const userEmail = req.session.passport.user;
+    const user = await User.findOne({ where: { email: userEmail } });
+
+    // Get houses that the user is a host of:
+    const houses = await House.findAll({
+      where: {
+        host: user.id,
+      },
+    });
+    // Get ids of the houses so we can look up their bookings:
+    const houseIds = houses.map((house) => house.dataValues.id);
+    // Get the bookings for any house that is in the houseIds array:
+    const bookingsData = await Booking.findAll({
+      where: {
+        paid: true,
+        houseId: {
+          [Op.in]: houseIds,
+        },
+        endDate: {
+          [Op.gte]: new Date(),
+        },
+      },
+      order: [["startDate", "ASC"]],
+    });
+
+    const bookings = await Promise.all(
+      bookingsData.map(async (booking) => {
+        return {
+          booking: booking.dataValues,
+          house: houses.filter(
+            (house) => house.dataValues.id === booking.dataValues.houseId
+          )[0].dataValues,
+        };
+      })
+    );
+
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(
+      JSON.stringify({
+        houses,
+      })
+    );
+  });
+
   server.all("*", (req, res) => {
     return handle(req, res);
   });
